6f4ade1070c2cb288a852c0ed0929f73fac1f438
赵成明 <zhaochengming@meituan.com>
2019-03-18 15:11:08 +0800

    add new and restrcutured Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- Essence_of_RL/Demo.py
+++ Essence_of_RL/Demo.py
@@ -4,16 +4,22 @@ This part of code followed book "the Essence of RL"
 """
 
 import numpy as np
+import pandas as pd
 import random
 from Snake import Snakes
 from Policy import PolicyIteration, ValueIteration, GeneralizedPolicyIteration
-from QLearning.MonteCarlo import MonteCarlo
+from QLearning.Strategy import MonteCarlo, SARSA, QLearning
 from Agent import TableAgent, eval_game, ModelFreeAgent
 from Utils import timer
+from _utils.u_constant import PATH_ROOT
+
+save_path = PATH_ROOT + "Code projects/Python/RL_Learn/Essence_of_RL/QLearning/"
+monte_carlo_samples_path = save_path + "monte_carlo_samples.txt"
+monte_carlo_samples_std_path = save_path + "monte_carlo_samples_std.txt"
 
 
 def alg_test(alg_name, alg, n_iters: int):
-    print("*" * 10)
+    print("*" * 20)
     sum_ = 0
     with timer(alg_name, 1):
         for _ in range(n_iters):
@@ -21,6 +27,13 @@ def alg_test(alg_name, alg, n_iters: int):
             sum_ += eval_game(env, alg.agent)
     print("%s policy avg. score: %.2f" % (alg_name, sum_ / n_iters))
     print(alg.agent.pi)
+
+
+def monte_carlo_std(sample_path):
+    df = pd.read_table(sample_path, names=["status", "act", "val"])
+    res = df.groupby(["status", "act"], as_index=False).agg({"val": ["count", "std"]})
+    res.columns = [x + "_" + y if y else x for x, y in res.columns]
+    return res
 
 
 if __name__ == '__main__':
@@ -44,12 +57,20 @@ if __name__ == '__main__':
         print("%s policy avg. score: %.2f" % (name, sum_ / eval_num))
 
     print("*" * 20)
+
     algs = {
         "PolicyIteration": PolicyIteration(TableAgent(env), max_iter=-1),
         "ValueIteration": ValueIteration(TableAgent(env), max_iter=-1),
         "GeneralizedPolicyIteration": GeneralizedPolicyIteration(TableAgent(env), max_policy_iter=1, max_value_iter=10),
-        "MonteCarlo": MonteCarlo(env, ModelFreeAgent(env), eval_iter=100, max_iter=10)
+        "MonteCarlo": MonteCarlo(env, ModelFreeAgent(env), eval_iter=100,  max_iter=10, episode_save=monte_carlo_samples_path),
+        "SARSA": SARSA(env, ModelFreeAgent(env), max_iter=10, eval_iter=100, epsilon=0.5),
+        "QLearning": QLearning(env, ModelFreeAgent(env), max_iter=10, eval_iter=100, epsilon=0.5)
     }
 
     for alg_name, alg in algs.items():
         alg_test(alg_name, alg, 100)
+
+    res = monte_carlo_std(monte_carlo_samples_path)
+    res.to_csv(monte_carlo_samples_std_path, index=False, sep="\t")
+
+
---
+++ Essence_of_RL/QLearning/Strategy.py
@@ -0,0 +1,147 @@
+
+"""
+This part of code followed book "the Essence of RL"
+"""
+
+import numpy as np
+import os
+from Policy import Alg
+from Snake import Snakes
+from Agent import ModelFreeAgent
+
+
+class MonteCarlo(Alg):
+    def __init__(self, env: Snakes, agent: ModelFreeAgent, *, epsilon=0.8, eval_iter=100, max_iter=10, episode_save=None):
+        self.env = env
+        self.agent = agent
+        self.agent.e_greedy = epsilon
+        self.eval_iter = eval_iter
+        self.max_iter = max_iter
+        self.episode_save = episode_save
+
+    def policy_eval(self):
+        for _ in range(self.eval_iter):
+            state = self.env.reset()
+            episode = []  # 记录状态行为序列，元素为三元组[si, ai, r]
+            done = False
+            while not done:
+                act = self.agent.play(state)
+                next_state, reward, done, _ = self.env.step(act)
+                episode.append((state, act, reward))
+                state = next_state
+
+            # 将每个状态的当期回报，变成预期回报（未来回报 * 各级筛减因子 + 当期回报）
+            return_val = 0.0
+            for i in range(len(episode) - 1, -1, -1):
+                return_val = self.agent.gamma * return_val + episode[i][2]
+                episode[i] = (episode[i][0], episode[i][1], return_val)
+
+            if self.episode_save is not None:
+                save_path = os.path.join(self.episode_save)
+                with open(save_path, "a+") as f:
+                    for state, act, return_val in episode:
+                        f.write("%d\t%d\t%.4f\n" % (state, act, return_val))
+
+            for state, act, return_val in episode:
+                self.agent.value_n[state, act] += 1
+                # 均值更新
+                self.agent.value_q[state, act] += \
+                    (return_val - self.agent.value_q[state, act]) / self.agent.value_n[state, act]
+
+    def policy_improve(self):
+        new_pi = np.argmax(self.agent.value_q, axis=1)
+        if np.all(np.equal(new_pi, self.agent.pi)):
+            return False
+        self.agent.pi = new_pi
+        return True
+
+    def iteration(self):
+        for _ in range(self.max_iter):
+            self.policy_eval()
+            self.policy_improve()
+
+
+class SARSA(Alg):
+    def __init__(self, env: Snakes, agent: ModelFreeAgent, *, epsilon=0.8, max_iter=10, eval_iter=200):
+        self.env = env
+        self.agent = agent
+        self.agent.e_greedy = epsilon
+        self.max_iter = max_iter
+        self.eval_iter = eval_iter
+
+    def policy_eval(self):
+        for _ in range(self.eval_iter):
+            state = self.env.reset()
+            prev_state = -1  # s
+            prev_act = -1  # a
+            done = False
+            while not done:
+                act = self.agent.play(state)  # state: s`, act: s`
+                next_state, reward, done, _ = self.env.step(act)  # reward: r(s`), next_state: new s`
+                if prev_act >= 0:
+                    # r(s`) + gamma * q(s`,a`)
+                    return_val = reward + self.agent.gamma * (0 if done else self.agent.value_q[state, act])
+                    self.agent.value_n[prev_state, prev_act] += 1
+
+                    # q(s, a) += {[r(s`) + gamma * q(s`, a`)] - q(s, a)} / N
+                    self.agent.value_q[prev_state, prev_act] += \
+                        (return_val - self.agent.value_q[prev_state, prev_act]) / self.agent.value_n[prev_state, prev_act]
+
+                prev_act = act
+                prev_state = state
+                state = next_state
+
+    def policy_improve(self):
+        new_pi = np.argmax(self.agent.value_q, axis=1)
+        if np.all(np.equal(new_pi, self.agent.pi)):
+            return False
+        self.agent.pi = new_pi
+        return True
+
+    def iteration(self):
+        for _ in range(self.max_iter):
+            self.policy_eval()
+            self.policy_improve()
+
+
+class QLearning(Alg):
+    def __init__(self, env: Snakes, agent: ModelFreeAgent, *, epsilon=0.8, max_iter=10, eval_iter=200):
+        self.env = env
+        self.agent = agent
+        self.agent.e_greedy = epsilon
+        self.max_iter = max_iter
+        self.eval_iter = eval_iter
+
+    def policy_eval(self):
+        for _ in range(self.eval_iter):
+            state = self.env.reset()
+            prev_state = -1  # s
+            prev_act = -1  # a
+            done = False
+            while not done:
+                act = self.agent.play(state)  # state: s`, act: s`
+                next_state, reward, done, _ = self.env.step(act)  # reward: r(s`), next_state: new s`
+                if prev_act >= 0:
+                    # r(s`) + gamma * max(q(s`,a`))
+                    return_val = reward + self.agent.gamma * (0 if done else self.agent.value_q[state, :].max())
+                    self.agent.value_n[prev_state, prev_act] += 1
+
+                    # q(s, a) += {[r(s`) + gamma * q(s`, a`)] - q(s, a)} / N
+                    self.agent.value_q[prev_state, prev_act] += \
+                        (return_val - self.agent.value_q[prev_state, prev_act]) / self.agent.value_n[prev_state, prev_act]
+
+                prev_act = act
+                prev_state = state
+                state = next_state
+
+    def policy_improve(self):
+        new_pi = np.argmax(self.agent.value_q, axis=1)
+        if np.all(np.equal(new_pi, self.agent.pi)):
+            return False
+        self.agent.pi = new_pi
+        return True
+
+    def iteration(self):
+        for _ in range(self.max_iter):
+            self.policy_eval()
+            self.policy_improve()
